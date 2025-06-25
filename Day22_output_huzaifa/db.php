995a22264136e67a977ed60d000b104c0c276554
huzaifashaikh-56 <hs7030342@gmail.com>
2025-06-25 19:57:48 +0530

    Update db.php

--- Day22_output_huzaifa/db.php
+++ Day22_output_huzaifa/db.php
@@ -1,1 +1,17 @@
 
+<?php
+// MySQL server info
+$servername = "localhost";
+$username = "root";        // Default for XAMPP/WAMP
+$password = "";            // Blank in XAMPP/WAMP
+$database = "webteam_intern";  // Your database name
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $database);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+echo "Connected successfully";
+?>
